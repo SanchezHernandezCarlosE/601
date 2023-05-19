ea39a5970365030cb94b02d7b2a6437511de8287
carlos emmanuel sanchez hernandez <elchemacarlos@gmail.com>
2023-05-19 11:17:17 -0600

    Se agrega funcion de V4TRIFIV

---
+++ 5 V4TRIFIV/src/funciones/funcionMultiple.java
@@ -0,0 +1,14 @@
+package funciones;
+
+public class funcionMultiple {
+    public String retornaMultiplo(int num){
+        if(num%3==0 && num%5==0){
+          return "TRIFIV";
+        } else if(num%3==0){
+          return"TRI";
+        } else if(num%5==0){
+          return"FIV";
+      }
+        return null;
+    }
+}
